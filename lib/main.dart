3ebf0e03145b98463118dfcdc9b06e9b45db964f
sankait36 <sankait36@hotmail.com>
2019-06-18 11:21:41 -0500

    Challenge begin

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+                  height: 100.0,
+                  color: Colors.red,
+                  child: Text('Container 1'),
+                ),
+                Container(
+                  height: 100.0,
+                  color: Colors.blue,
+                  child: Text('Container 3'),
+                ),
+              ],
+            ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
+
