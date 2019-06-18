5dabbcf865bda111b4a5cf64c8f913476ac248c5
sankait36 <sankait36@hotmail.com>
2019-06-18 11:24:51 -0500

    Challenge complete

--- lib/main.dart
+++ lib/main.dart
@@ -16,14 +16,29 @@ class MyApp extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
                 Container(
-                  height: 100.0,
+                  width: 100.0,
                   color: Colors.red,
-                  child: Text('Container 1'),
                 ),
                 Container(
-                  height: 100.0,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        width: 100.0,
+                        height: 100.0,
+                        color: Colors.yellow,
+                      ),
+                      Container(
+                        width: 100.0,
+                        height: 100.0,
+                        color: Colors.green,
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 100.0,
                   color: Colors.blue,
-                  child: Text('Container 3'),
                 ),
               ],
             ),
